23cef1bfaab7d1b64566659415afd17904c9bf63
Robert Beach <surfdemon@gmail.com>
2024-10-09 10:31:12 +0000

    Maint: Add update score function`

--- assets/js/score.js
+++ assets/js/score.js
@@ -1,1 +1,11 @@
 console.log('score.js loaded');
+
+const scoreElement = document.getElementById('score');
+let score = scoreElement.innerText;
+
+const updateScore = () => { 
+    scoreElement.innerText = ++score;
+}
+
+
+window.updateGameScore = updateScore;
